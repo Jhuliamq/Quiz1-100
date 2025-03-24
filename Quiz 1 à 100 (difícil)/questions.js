0b94eecda841d33c100db219ac454b66b222dd92
Jhulia <127256522+Jhuliamq@users.noreply.github.com>
2025-03-24 15:18:42 -0300

    Add files via upload

---
+++ Quiz 1 à 100 (difícil)/questions.js
@@ -0,0 +1,92 @@
+const questao = [
+    {
+      question: "Qual número está localizado no meio da sequência de 1 à 100 ?",
+      answers: [
+        { option: "50", correct: true},
+        { option: "10", correct: false},
+        { option: "40", correct: false},
+        { option: "35", correct: false},
+      ],
+    },
+    {
+      question: "Qual número vem depois do 28 ?",
+      answers: [
+        { option: "1", correct: false},
+        { option: "29", correct: true},
+        { option: "30", correct: false},
+        { option: "80", correct: false},
+      ],
+    },
+    {
+      question: "Qual número vem antes do 80 ?",
+      answers: [
+        { option: "10", correct: false},
+        { option: "79", correct: true},
+        { option: "90", correct: false},
+        { option: "100", correct: false},
+      ],
+    },
+    {
+      question: "Qual número vem antes do 100 ?",
+      answers: [
+        { option: "110", correct: false},
+        { option: "99", correct: true},
+        { option: "101", correct: false},
+        { option: "50", correct: false},
+      ],
+    },
+    {
+      question: "Qual o último número da ordem de 1 à 100 ?",
+      answers: [
+        { option: "10", correct: false},
+        { option: "100", correct: true},
+        { option: "101", correct: false },
+        { option: "15", correct: false},
+      ],
+    },
+    {
+      question: "Quantas dezenas existem no número 5 ?",
+      answers: [
+        { option: "1", correct: false},
+        { option: "6", correct: false},
+        { option: "0", correct: true},
+        { option: "5", correct: false},
+      ],
+    },
+    {
+      question: "Quantas unidade existem no número 20 ?",
+      answers: [
+        { option: "20", correct: true},
+        { option: "22", correct: false} ,
+        { option: "15 ", correct: false},
+        { option: "40", correct: false},
+      ],
+    },
+    {
+      question: "Quantas dezenas existem no número 21 ?",
+      answers: [
+        { option: "5 ", correct: false},
+        { option: "20 ", correct: false},
+        { option: "1", correct: false},
+        { option: "2", correct: true},
+      ],
+    },
+    {
+      question: "Quantas centenas existem no número 115 ?",
+      answers: [
+        { option: "1 ", correct: true},
+        { option: "5 ", correct: false},
+        { option: "15 ", correct: false},
+        { option: "9 ", correct: false},
+      ],
+    },
+    {
+      question:"Quantas dezenas tem no numero 220 ?",
+      answers: [
+        { option:"22", correct: true},
+        { option:"21", correct: false},
+        { option:"10", correct: false},
+        { option:"20", correct: false},
+      ],
+    },
+  ];
